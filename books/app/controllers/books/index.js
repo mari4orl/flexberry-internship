775b460670603a6f1d112f0faf6780787c107085
mari4orl <mari4orl@mail.ru>
2022-05-18 12:34:55 +0300

    Try to make search by click button

--- books/app/controllers/books/index.js
+++ books/app/controllers/books/index.js
@@ -2,11 +2,15 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  queryParams: ['search', 'searchByTag'],
-  search: '',
-  searchByTag: '',
+  // queryParams: ['search', 'searchByTag'],
+  // search: '',
+  // searchByTag: '',
   dataService: service('data'),
   actions: {
+    searchBooks(search) {
+      this.queryParams = search;
+      search = '';
+    },
     async deleteBook(book) {
       try {
         await this.get('dataService').deleteBook(book);
